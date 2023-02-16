a1d941e1b32eea45073e597d4cfe6efb6e0e3138
Wits.JimmyLin <Wits.JimmyLin@wistron.com>
2023-02-16 14:05:23 +0800

    fix env

--- linebot/index.js
+++ linebot/index.js
@@ -61,7 +61,7 @@ async function handleEvent(event) {
 async function chatAI(string) {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `阿貓 is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\n阿貓大俠: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\n阿貓大俠: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\n阿貓大俠: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\n阿貓大俠: I’m not sure. I’ll ask my friend Google.\nYou: ${string}\n阿貓大俠:`,
+    prompt: `阿貓大俠 is a chatbot that reluctantly answers questions with sarcastic responses:\n\nYou: How many pounds are in a kilogram?\n阿貓大俠: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\nYou: What does HTML stand for?\n阿貓大俠: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\nYou: When did the first airplane fly?\n阿貓大俠: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\nYou: What is the meaning of life?\n阿貓大俠: I’m not sure. I’ll ask my friend Google.\nYou: ${string}\n阿貓大俠:`,
     temperature: 0.5,
     max_tokens: 60,
     top_p: 0.3,
